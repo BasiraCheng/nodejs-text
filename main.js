ac4354972dd6c70d6137df9c7cb712b2802df502
BasiraCheng <2249639871@qq.com>
2019-06-14 10:08:46 +0800

    提交代码

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
 window.jQuery = {}
-window.jQuery.ajax = function({url,method,body,successFn,failFn,headers}){
-    
-    let request = new XMLHttpRequest()
-    request.open(method,url)
-    for(let key in headers){
-        let value = headers[key]
-        request.setRequestHeader(key,value)
-    }
-    request.onreadystatechange  = ()=>{
-        if(request.readyState === 4){
-            if(request.status >= 200 && request.status < 300){
-                successFn.call(undefined,request.responseText)
-            }else if(request.status >= 400){
-                failFn.call(undefined,request)
+window.jQuery.ajax = function({url,method,body,headers}){
+    return new Promise(function(resolve,reject){//成功调第一个，失败调第二个
+        let request = new XMLHttpRequest()
+        request.open(method,url)
+        for(let key in headers){
+            let value = headers[key]
+            request.setRequestHeader(key,value)
+        }
+        request.onreadystatechange  = ()=>{
+            if(request.readyState === 4){
+                if(request.status >= 200 && request.status < 300){
+                    resolve.call(undefined,request.responseText)
+                }else if(request.status >= 400){
+                    reject.call(undefined,request)
+                }
             }
         }
-    }
-    request.send(body)
+        request.send(body)
+    })
 }
-function f1(responseText){}
-function f2(responseText){}
 
 myButton.addEventListener('click',(e)=>{
     window.jQuery.ajax({
@@ -28,73 +27,9 @@ myButton.addEventListener('click',(e)=>{
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
             'frank': '18'
-        }, 
-        
-        successFn: (x) =>{
-            f1.call(undefined,x)
-            f2.call(undefined,x) 
-        },
-        failFn: (x) =>{
-            console.log(x)
-            console.log(status)
-            console.log(x.responseText)
-
         }
-    })
+    }).then(
+        (text)=>{console.log(text)},
+        (request)=>{console.log(request)}
+    )
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* myButton.addEventListener('click',(e)=>{
-    let request = new XMLHttpRequest()
-    request.open('get','http://jack.com:8002/xxx')
-
-    request.send()
-    request.onreadystatechange = ()=>{
-       if(request.readyState === 4){
-          if(request.status >= 200 && request.status < 300){
-              console.log('说明请求成功')
-             console.log(typeof request.responseText)
-             console.log(request.responseText)
-             let string = request.responseText
-             //把符合JSON语法的字符串
-             //转换成JS对应的值
-             let object = window.JSON.parse(string)
-             console.log(typeof object)
-             console.log(object)
-             console.log('object.note')
-             console.log(object.note)
-          }else if(request.status >= 400){
-              console.log('说明请求失败')
-          }
-       }
-    }
-}) */
--- server.js
+++ server.js
@@ -3,16 +3,16 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if(!port){
+if (!port) {
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
   var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
+  var pathWithQuery = request.url
   var queryString = ''
-  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
   var path = parsedUrl.pathname
   var query = parsedUrl.query
   var method = request.method
@@ -21,23 +21,101 @@ var server = http.createServer(function(request, response){
 
   console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
-  if(path === '/'){
-    let string = fs.readFileSync('./index.html','utf8')
+  if (path === '/') {
+    let string = fs.readFileSync('./index.html', 'utf8')
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(string)
     response.end()
-  }else if(path === '/main.js'){
-    let string = fs.readFileSync('./main.js','utf8')
+  } else if (path === '/sign_up' && method === 'GET') {
+    let string = fs.readFileSync('./sign_up.html', 'utf8')
     response.statusCode = 200
-    response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(string)
     response.end()
-  }else if(path ==='/xxx'){
+  } else if (path === '/sign_up' && method === 'POST') {
+    readBody(request).then((body) => {
+      let strings = body.split('&') //字符串分开['email=1','password=3','password_confirmation=2']
+      let hash = {}
+      strings.forEach((string) => {
+        let parts = string.split('=')//['email','1']
+        let key = parts[0]
+        let value = parts[1]
+        hash[key] = decodeURIComponent(value)//hash['email'] = '1'
+      })
+      let { email, password, password_confirmation } = hash
+      if (email.indexOf('@') === -1) {
+        response.statusCode = 400
+        response.setHeader('Content-Type', 'application/json;charset=utf-8')
+        response.write(`{
+          "errors": {
+            "email": "invalid"
+          }
+        }`)
+      } else if (password !== password_confirmation) {
+        response.statusCode = 400
+        response.write('Password not match')
+      } else {
+        var users = fs.writeFileSync('./db/users', 'utf8')
+        try {
+          users = JSON.parse(users)
+        } catch (exception) {
+          users = []
+        }
+        let inuse = false
+        for (let i = 0; i < users.length; i++) {
+          let user = users[i]
+          if (user.email === email) {
+            inuse = true
+            break;
+          }
+        }
+        if (inuse) {
+          response.statusCode = 400
+          response.write('Email in use')
+        } else {
+          users.push({ email: email, password: password })
+          var usersString = JSON.stringify(users)
+          fs.writeFileSync('./db/users', usersString)
+          response.statusCode = 200
+        }
+      }
+      response.end()
+    })
+  } else if (path === '/sign_in' && method === 'GET') {
+    let string = fs.readFileSync('./sign_in.html', 'utf8')
     response.statusCode = 200
-    response.setHeader('Content-Type', 'text/json;charset=utf-8')
-    response.setHeader('Access-Control-Allow-Origin', 'http://frank.com:8001') //这句很重要
-    response.write(`
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(string)
+    response.end()
+  } else if (path === '/sign_in' && method === 'POST') {
+    readBody(request).then((body) => {
+      let strings = body.split('&') //字符串分开['email=1','password=3','password_confirmation=2']
+      let hash = {}
+      strings.forEach((string) => {
+        let parts = string.split('=')//['email','1']
+        let key = parts[0]
+        let value = parts[1]
+        hash[key] = decodeURIComponent(value)//hash['email'] = '1'
+      })
+      let { email, password } = hash
+      console.log('email')
+      console.log(email)
+      console.log('password')
+      console.log(password)
+      response.end()
+    })
+  }else if (path === '/main.js') {
+  let string = fs.readFileSync('./main.js', 'utf8')
+  response.statusCode = 200
+  response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+  response.write(string)
+  response.end()
+} else if (path === '/xxx') {
+  response.statusCode = 200
+  response.setHeader('Content-Type', 'text/json;charset=utf-8')
+  response.setHeader('Access-Control-Allow-Origin', 'http://frank.com:8001') //这句很重要
+  response.write(`
     {
       "note": {
         "to": "Alen",
@@ -47,20 +125,31 @@ var server = http.createServer(function(request, response){
       }
     } 
     }`)
-    response.end()
-  }else{
-    response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`
+  response.end()
+} else {
+  response.statusCode = 404
+  response.setHeader('Content-Type', 'text/html;charset=utf-8')
+  response.write(`
     {
       "error": "not found"
     }
     `)
-    response.end()
-  }
+  response.end()
+}
 
   /******** 代码结束，下面不要看 ************/
 })
+function readBody(request) {
+  return new Promise((resolve, reject) => {
+    let body = []
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      resolve(body)
+    })
+  })
+}
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
